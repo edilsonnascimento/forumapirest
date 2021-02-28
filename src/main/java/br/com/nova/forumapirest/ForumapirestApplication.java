53d06d1c6d4eb75822aa1a65c2032059d9edbaee
unknown <Nova@NOVAJFLAP057.novatendencia.localdomain>
2021-02-28 09:50:17 -0300

    Criando projeto SpringBoot

---
+++ src/main/java/br/com/nova/forumapirest/ForumapirestApplication.java
@@ -0,0 +1,13 @@
+package br.com.nova.forumapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumapirestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumapirestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/nova/forumapirest/ForumapirestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nova.forumapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumapirestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
