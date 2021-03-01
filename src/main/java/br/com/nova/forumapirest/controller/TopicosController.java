bc95d7122a493272054e6294133b528f13894bdf
unknown <Nova@NOVAJFLAP057.novatendencia.localdomain>
2021-03-01 15:21:06 -0300

    Adicionado validação

--- src/main/java/br/com/nova/forumapirest/controller/TopicosController.java
+++ src/main/java/br/com/nova/forumapirest/controller/TopicosController.java
@@ -6,15 +6,12 @@ import br.com.nova.forumapirest.modelo.Topico;
 import br.com.nova.forumapirest.repository.CursoRepository;
 import br.com.nova.forumapirest.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.persistence.GeneratedValue;
+import javax.validation.Valid;
 import java.net.URI;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -27,7 +24,7 @@ public class TopicosController {
     @Autowired
     private CursoRepository cursoRepository;
 
-    @GeneratedValue
+    @GetMapping
     public List<TopicoDTO> lista(String nomeCurso){
         if(nomeCurso == null) {
             return TopicoDTO.converter(topicoRepository.findAll());
@@ -37,7 +34,7 @@ public class TopicosController {
     }
 
     @PostMapping
-    public ResponseEntity<TopicoDTO> cadastrar(@RequestBody TopicoForm topicoForm, UriComponentsBuilder uriComponentsBuilder){
+    public ResponseEntity<TopicoDTO> cadastrar(@RequestBody @Valid TopicoForm topicoForm, UriComponentsBuilder uriComponentsBuilder){
         Topico topico = topicoForm.converter(cursoRepository);
         topicoRepository.save(topico);
 
--- src/main/java/br/com/nova/forumapirest/controller/form/TopicoForm.java
+++ src/main/java/br/com/nova/forumapirest/controller/form/TopicoForm.java
@@ -3,11 +3,18 @@ package br.com.nova.forumapirest.controller.form;
 import br.com.nova.forumapirest.modelo.Curso;
 import br.com.nova.forumapirest.modelo.Topico;
 import br.com.nova.forumapirest.repository.CursoRepository;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 public class TopicoForm {
 
+    @NotNull @NotEmpty @Length(min = 5)
     private String titulo;
+    @NotNull @NotEmpty @Length(min = 10)
     private String mensagem;
+    @NotNull @NotEmpty
     private String nomeCurso;
 
     public String getTitulo() {
