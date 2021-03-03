2f569094eeda0cabf0d43461eb30589379d4b0d3
unknown <Nova@NOVAJFLAP057.novatendencia.localdomain>
2021-03-03 08:50:15 -0300

    Adicionado Ordenação na lista paginada

--- src/main/java/br/com/nova/forumapirest/controller/TopicosController.java
+++ src/main/java/br/com/nova/forumapirest/controller/TopicosController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -35,9 +36,10 @@ public class TopicosController {
     private Topico topico;
 
     @GetMapping
-    public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int quantidade){
+    public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int quantidade, @RequestParam String ordenacao){
 
-        Pageable paginacao = PageRequest.of(pagina, quantidade);
+        Pageable paginacao = PageRequest.of(pagina, quantidade, Sort.Direction.DESC, ordenacao);
+
 
         if(nomeCurso == null) {
             Page<Topico> topicos = topicoRepository.findAll(paginacao);
