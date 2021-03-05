1667e085d0250f65e6cc46daf7a5c65b59897e0f
unknown <Nova@NOVAJFLAP057.novatendencia.localdomain>
2021-03-05 08:51:52 -0300

    Adicionado cache para listar

--- src/main/java/br/com/nova/forumapirest/controller/TopicosController.java
+++ src/main/java/br/com/nova/forumapirest/controller/TopicosController.java
@@ -8,10 +8,12 @@ import br.com.nova.forumapirest.modelo.Topico;
 import br.com.nova.forumapirest.repository.CursoRepository;
 import br.com.nova.forumapirest.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -36,10 +38,8 @@ public class TopicosController {
     private Topico topico;
 
     @GetMapping
-    public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int quantidade, @RequestParam String ordenacao){
-
-        Pageable paginacao = PageRequest.of(pagina, quantidade, Sort.Direction.DESC, ordenacao);
-
+    @Cacheable(value = "listaTopicos")
+    public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso, @PageableDefault(sort = "id", direction = Sort.Direction.DESC, page = 0, size = 2) Pageable paginacao){
 
         if(nomeCurso == null) {
             Page<Topico> topicos = topicoRepository.findAll(paginacao);
